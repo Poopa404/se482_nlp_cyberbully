5985a08cedc8afae6cb293d0d9ee4955587e4aff
NOHP404 <poopa_somth@cmu.ac.th>
2024-03-07 06:36:22 +0700

    add stopwords

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import warnings
 import pandas as pd
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
